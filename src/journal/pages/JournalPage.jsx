c52152a9c329d638b80908d55eef55443ef073fc
leon <leonchorroarin@gmail.com>
2023-07-28 22:26:25 -0300

    actualizaciones

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,9 +1,15 @@
 import { Typography } from '@mui/material'
+import { JournalLayout } from '../layout/JournalLayout'
 
 export const JournalPage = () => {
     return (
-        <>
-            <Typography variant='h1'>JournalPage</Typography>
-        </>
+        <JournalLayout>
+            
+            <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium quo ducimus culpa totam voluptate adipisci tempore quisquam nam, fugit magnam rem ex itaque ea repellendus, ipsam quaerat sint doloribus dolorem.</Typography>
+        
+            {/* NothingSelected */}
+            {/* NoteView */}
+
+        </JournalLayout>
     )
 }
