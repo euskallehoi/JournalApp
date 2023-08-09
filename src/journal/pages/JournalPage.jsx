01a2ca2bd6f67317c621223a5aaf05688f91670f
leon <leonchorroarin@gmail.com>
2023-08-09 16:12:13 -0300

    cascaron terminado

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -9,8 +9,8 @@ export const JournalPage = () => {
             
             {/* <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium quo ducimus culpa totam voluptate adipisci tempore quisquam nam, fugit magnam rem ex itaque ea repellendus, ipsam quaerat sint doloribus dolorem.</Typography> */}
         
-            {/* <NothingSelectedView /> */}
-            <NoteView />
+            <NothingSelectedView />
+            {/* <NoteView /> */}
 
             <IconButton
                 size='large'
